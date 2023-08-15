8b1f6a869ed5d4066e710d343b54e28aac7830df
Alexandre Mommers <none@ygdrasil.io>
2023-08-15 16:51:12 +0200

    download mac SDK

---
+++ sdl.main.kts
@@ -0,0 +1,47 @@
+#!/usr/bin/env kotlinc -script -J-Xmx2g
+
+import java.io.*
+import java.net.URL
+
+
+val fileUrl = "https://github.com/libsdl-org/SDL/releases/download/release-2.28.2/SDL2-2.28.2.dmg"
+val fileName = "./tmp/temp.bin"
+downloadFile(fileUrl, fileName)
+"hdiutil attach $fileName".run {
+
+}
+"cp -R /Volumes/SDL2/SDL2.framework ./tmp/SDL2".run {
+
+}
+
+
+fun downloadFile(fileUrl: String, fileName: String) {
+    File(fileName).parentFile.mkdirs()
+    BufferedInputStream(URL(fileUrl).openStream()).use { bufferedInputStream ->
+        FileOutputStream(fileName).use { fileOutputStream ->
+            val dataBuffer = ByteArray(1024)
+            var bytesRead: Int
+            while (bufferedInputStream.read(dataBuffer, 0, 1024).also { bytesRead = it } != -1) {
+                fileOutputStream.write(dataBuffer, 0, bytesRead)
+            }
+        }
+    }
+
+}
+
+fun String.run(onError: (String) -> Unit) {
+    val process = ProcessBuilder()
+        .command("bash", "-c", this)
+        .start()
+
+    val reader = BufferedReader(InputStreamReader(process.errorStream))
+    val errors = mutableListOf<String>()
+    var line: String?
+    while (reader.readLine().also { line = it } != null) {
+        errors.add(line ?: "")
+    }
+
+    if (errors.isNotEmpty()) {
+        onError(errors.joinToString("\n"))
+    }
+}
