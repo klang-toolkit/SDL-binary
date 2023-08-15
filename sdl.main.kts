c3a33715f20aba3aa5cb4cc6e4505378536c92c6
Alexandre Mommers <none@ygdrasil.io>
2023-08-15 17:11:56 +0200

    try something

--- sdl.main.kts
+++ sdl.main.kts
@@ -7,13 +7,9 @@ import java.net.URL
 val fileUrl = "https://github.com/libsdl-org/SDL/releases/download/release-2.28.2/SDL2-2.28.2.dmg"
 val fileName = "./tmp/temp.bin"
 downloadFile(fileUrl, fileName)
-"hdiutil attach $fileName".run {
-
-}
-"cp -R /Volumes/SDL2/SDL2.framework ./tmp/SDL2".run {
-
-}
-
+"hdiutil attach $fileName".run { println(it) }
+"cp -R /Volumes/SDL2/SDL2.framework ./tmp/SDL2".run { println(it) }
+"zip -r ./tmp/headers.zip ./tmp/SDL2/Versions/A/Headers".run { println(it) }
 
 fun downloadFile(fileUrl: String, fileName: String) {
     File(fileName).parentFile.mkdirs()
